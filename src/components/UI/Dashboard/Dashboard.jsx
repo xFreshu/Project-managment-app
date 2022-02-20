302b85bafd786ca27cc66a5aa054827f1f7ba60e
DESKTOP-EJMKT25\Lukasz <lprzybyszcontact@gmail.com>
2022-02-20 20:55:13 +0100

    Working on App Dashboard

--- src/components/UI/Dashboard/Dashboard.jsx
+++ src/components/UI/Dashboard/Dashboard.jsx
@@ -5,6 +5,70 @@ import Navigation from '../Navigation/Navigation';
 import { Wrapper } from './Dashboard.styles';
 import { AppContext } from '../../../providers/AppProvider';
 import TicketContainer from '../TicketContainer/TicketContainer';
+import styled from 'styled-components';
+
+const DashboardCard = styled.div`
+  background-color: grey;
+  border-radius: 20px;
+  display: flex;
+  width: 60%;
+  justify-content: space-between;
+  padding: 20px;
+  height: 89vh;
+  div {
+    :nth-child(1) {
+      position: relative;
+      width: 30%;
+      ul {
+        list-style: none;
+      }
+    }
+    :nth-child(2) {
+      position: relative;
+      background-color: red;
+      width: 65%;
+      :after {
+        position: absolute;
+        left: 0;
+        content: '';
+        display: inline-block;
+        width: 4px;
+        height: 200px;
+        top: 50%;
+        transform: translateY(-50%);
+        background-color: black;
+        border-radius: 20px;
+      }
+    }
+  }
+`;
+const StyledEnv = styled.span`
+  font-weight: 700;
+  color: ${({ value }) => {
+    if (value === 'prod') return '#f44336';
+    if (value === 'test') return '#03a9f4';
+    if (value === 'dev') return '#4caf50';
+  }};
+  background-color: ${({ value }) => {
+    if (value === 'prod') return 'rgba(244,67,54,0.3)';
+    if (value === 'test') return 'rgba(3,169,244,0.3)';
+    if (value === 'dev') return 'rgba(76,175,80,0.3)';
+  }};
+  padding: 4px 7px;
+  border-radius: 10px;
+`;
+
+const TicketCard = styled.li`
+  background-color: #fff;
+  margin: 20px 0;
+  div {
+    :nth-child(1) {
+      :after {
+        display: none;
+      }
+    }
+  }
+`;
 
 const Dashboard = () => {
   let { userData, tickets } = useContext(AppContext);
@@ -14,27 +78,44 @@ const Dashboard = () => {
     <>
       <Navigation userData={userData} />
       <Wrapper>
-        {userData.role === 'PM' ? <TicketForm /> : null}
-        {userData.role !== 'client' ? (
-          getAssignedArray.length > 0 ? (
-            getAssignedArray.map(({ id, name, notes, environment, deadline }) => (
-              <TicketContainer
-                id={id}
-                name={name}
-                notes={notes}
-                environment={environment}
-                deadline={deadline}
-              />
-            ))
-          ) : (
-            <div>Your login do not have permission for that application :(</div>
-          )
-        ) : (
-          <BasicTable />
-        )}
+        <DashboardCard>
+          <div>
+            <ul>
+              {tickets.map(({ id, name, notes, environment, deadline }) => (
+                <TicketCard>
+                  <div>
+                    <span>{name}</span>
+                    <span>{id}</span>
+                  </div>
+                </TicketCard>
+              ))}
+            </ul>
+          </div>
+          <div>Ticket display</div>
+        </DashboardCard>
+        <div>Widget pie chart</div>
       </Wrapper>
     </>
   );
 };
 
 export default Dashboard;
+
+// {userData.role === 'PM' ? <TicketForm /> : null}
+// {userData.role !== 'client' ? (
+//   getAssignedArray.length > 0 ? (
+//     getAssignedArray.map(({ id, name, notes, environment, deadline }) => (
+//       <TicketContainer
+//         id={id}
+//         name={name}
+//         notes={notes}
+//         environment={environment}
+//         deadline={deadline}
+//       />
+//     ))
+//   ) : (
+//     <div>Your login do not have permission for that application :(</div>
+//   )
+// ) : (
+//   <BasicTable />
+// )}
--- src/components/UI/Dashboard/Dashboard.styles.js
+++ src/components/UI/Dashboard/Dashboard.styles.js
@@ -1,9 +1,10 @@
 import styled from 'styled-components';
 
 export const Wrapper = styled.div`
+  padding: 20px;
   display: flex;
-  flex-direction: column;
-  align-content: center;
-  justify-content: center;
-  width: 100%;
+  flex-direction: row;
+  justify-content: space-between;
+  height: 100%;
+  align-items: center;
 `;
